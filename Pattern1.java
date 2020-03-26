8f655f1af04bdabe9f7e7050ea9e269eedf44bf3
BharkaviS <41267360+BharkaviS@users.noreply.github.com>
2020-03-26 11:47:17 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,32 @@
+/*
+Input:5
+
+Output:
+
+1 1 1 1 1
+2 2 2 2
+3 3 3
+4 4
+5
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+
+public class Pattern1
+{
+	public static void main(String[] args) {
+		int n,i,j,k=1;
+		Scanner sc = new Scanner(System.in);
+		n=sc.nextInt();
+		for(i=0;i<n;i++){
+		    for(j=n;j>i;j--){
+		        System.out.print(k+" ");
+		    }
+		    k++;
+		    System.out.println(" ");
+		}
+	}
+}
